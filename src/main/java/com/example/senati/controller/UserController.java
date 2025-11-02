6fa3e02b10c8c2e29f82d677efb149dd968f4d02
jair-angel777 <1636763@senati.pe>
2025-11-02 09:20:36 -0500

    origins

--- src/main/java/com/example/senati/controller/UserController.java
+++ src/main/java/com/example/senati/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin("http://localhost:4200/")
+@CrossOrigin("https://entregabledgamc-cabkh8d8aha8fbft.chilecentral-01.azurewebsites.net")
 public class UserController {
     @Autowired
     UserService userService;
@@ -29,7 +29,7 @@ public class UserController {
         return userService.newUser(user);
     }
 
-    @DeleteMapping("/api/users{id}")
+    @DeleteMapping("/api/users/{id}")
     public Response deleteUser(@PathVariable int id){
         return  userService.deleteUser(id);
     }
@@ -38,7 +38,7 @@ public class UserController {
     // return ResponseEntity.noContent().build();
     //}
 
-    @PutMapping ("/api/users{id}")
+    @PutMapping ("/api/users/{id}")
     public ResponseEntity<User> newUser(@PathVariable int id,@RequestBody User user){
         return userService.updateUser(id, user);
     }
