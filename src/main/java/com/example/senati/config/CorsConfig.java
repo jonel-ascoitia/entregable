24ef4df27bd067d9bb4c78168a9fb84021f293d8
jair-angel777 <1636763@senati.pe>
2025-11-02 11:39:30 -0500

    intento1paradelete

---
+++ src/main/java/com/example/senati/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.example.senati.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("https://entregabledgamc-cabkh8d8aha8fbft.chilecentral-01.azurewebsites.net") // tu frontend
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
+
